2b3d1dcc7ef95e2569f783dd15b79cf3f1f3915a
popenc <i.nickpope@gmail.com>
2019-11-05 11:33:26 -0500

    updated cyan_angular flask url for development and production/docker using angular's environments feature

--- cyan_angular/src/environments/environment.ts
+++ cyan_angular/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseServerUrl: 'http://127.0.0.1:5001/cyan/app/api/'
 };
 
 /*
