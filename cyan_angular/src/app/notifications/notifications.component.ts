2769c79800ae41ce7a208000f13c7af27bc3c7d7
popenc <i.nickpope@gmail.com>
2019-12-11 09:42:22 -0500

    added notifications component

--- cyan_angular/src/app/notifications/notifications.component.ts
+++ cyan_angular/src/app/notifications/notifications.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import { Location } from '../models/location';
+// import { LocationService } from '../services/location.service';
+// import { MapService } from '../services/map.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-notifications',
@@ -7,9 +15,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationsComponent implements OnInit {
 
-  constructor() { }
+	new_notifications = [];
+	notificationSubscription: Subscription;
+
+  constructor(
+		// private router: Router,
+		private userService: UserService
+  ) { }
 
   ngOnInit() {
+
+		this.new_notifications = this.userService.currentAccount.notifications;
+
+  	// Is this needed?
+		this.notificationSubscription = this.userService.allNotifications$.subscribe(
+			notifications => {
+				console.log("Received notifications update in notifications component.");
+				this.new_notifications = notifications;
+			}
+		);
+  }
+
+  ngOnDestroy() {
+		this.notificationSubscription.unsubscribe();
+  }
+
+  toggleChecked() {
+  }
+
+  hasNotifications() {
+  }
+
+  notificationSelect() {
+  	/*
+  	Check status of notification and set bool based on
+  	is_new Notification attribute.
+  	*/
+  	
   }
 
 }
