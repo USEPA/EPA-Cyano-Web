cc130ba19a2819e61ac59635b5402c8381fb4653
popenc <i.nickpope@gmail.com>
2019-12-11 09:42:04 -0500

    added notifications feature to header, and notifications subscriber

--- cyan_angular/src/app/header/header.component.ts
+++ cyan_angular/src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, NgModule } from '@angular/core';
+import { LocationService } from '../services/location.service';
+import { UserService } from '../services/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -10,9 +13,37 @@ export class HeaderComponent implements OnInit {
   title = "Cyanobacteria Assessment Network";
   titleAbr = "CyAN";
 
-  constructor() { }
+  new_notifications = [];
+  notificationSubscription: Subscription;
+
+  constructor(
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
+   //  let self = this;
+  	// setTimeout(function() {
+   //    self.userService.getAllNotifications();
+   //  }, 100);
+
+  	// Does this subscrition make sense?
+    this.notificationSubscription = this.userService.allNotifications$.subscribe(
+      	notifications => {
+        	console.log("Received notifications update in header component.");	
+        	this.new_notifications = notifications;
+     	}
+     );
+
+    // this.notificationService.getAllNotifications().subscribe(
+    // 	notifications => {
+	   //      console.log("Notifications: ");
+	   //      console.log(notifications);
+    // 	}
+    // );
+  }
+
+  ngOnDestroy() {
+    this.notificationSubscription.unsubscribe();
   }
 
 
