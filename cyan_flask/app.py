41841c921948da3bac6a8a2ec3f6428bf4a838a0
popenc <i.nickpope@gmail.com>
2019-12-11 09:40:12 -0500

    added notifications endpoints to flask backend

--- cyan_flask/app.py
+++ cyan_flask/app.py
@@ -166,6 +166,30 @@ class GetLocation(Resource):
 		results = simplejson.loads(simplejson.dumps(result_obj))
 		return results, status_code
 
+class AddNotification(Resource):
+	"""
+	Endpoint for adding notification from last sync time.
+	"""
+	pass
+
+class EditNotification(Resource):
+	"""
+	Endpoint for setting is_new false after read.
+	"""
+	pass
+
+class DeleteNotification(Resource):
+	"""
+	Endpoint for "Clear" notifications.
+	"""
+	pass
+
+class GetNotification(Resource):
+	"""
+	Endpoint for populating notifications list.
+	"""
+	pass
+
 
 
 api.add_resource(StatusTest, '/test')
@@ -176,6 +200,15 @@ api.add_resource(AddLocation, '/cyan/app/api/location/add')
 api.add_resource(EditLocation, '/cyan/app/api/location/edit')
 api.add_resource(DeleteLocation, '/cyan/app/api/location/delete/<string:user>/<string:_id>')
 api.add_resource(GetLocation, '/cyan/app/api/location/<string:user>/<string:_id>')
+
+# Notifications Endpoints:
+api.add_resource(AddNotification, '/cyan/app/api/notification/add')
+api.add_resource(EditNotification, '/cyan/app/api/notification/edit')
+api.add_resource(DeleteNotification, '/cyan/app/api/notification/delete/<string:user>/<string:_id>')
+api.add_resource(GetNotification, '/cyan/app/api/notification/<string:user>/<string:_id>')
+
+
+
 
 logging.info("CyAN Flask app started.\nLive endpoints:")
 logging.info(base_url + '/test')
--- cyan_flask/web_app_api.py
+++ cyan_flask/web_app_api.py
@@ -7,12 +7,14 @@ Handles user account interactions.
 import hashlib
 import binascii
 import uuid
+import time
 import datetime
 import sqlite3
 import mysql.connector
 import json
 import os
 import logging
+import requests
 
 
 
@@ -27,15 +29,13 @@ def hash_password(password):
 	password_salted = (salt + password_hex).decode('ascii')
 	return password_salted
 
-
 def test_password(password_0, password_1):
 	salt = password_0[:64]
 	password_1_hash = hashlib.pbkdf2_hmac('sha512', password_1.encode('utf-8'), salt.encode('ascii'), 100000)
 	password_1_hex = binascii.hexlify(password_1_hash).decode('ascii')
 	return password_0[64:] == password_1_hex
 
-
-def query_database(query, values):
+def query_database(query, values, execution_type=None):
 	conn = mysql.connector.connect(
 		host=os.environ.get('DB_HOST'),
 		port=os.environ.get('DB_PORT'),
@@ -45,7 +45,10 @@ def query_database(query, values):
 	)
 	c = conn.cursor()
 	try:
-		c.execute(query, values)
+		if execution_type == 'multi':
+			c.executemany(query, values)
+		else:
+			c.execute(query, values)
 	except mysql.connector.Error as e:
 		conn.close()
 		logging.warning("query_database error: {}".format(e))
@@ -60,7 +63,7 @@ def query_database(query, values):
 	return results
 
 
-# @csrf_exempt
+
 def register_user(post_data):
 	try:
 		user = post_data['user']
@@ -81,15 +84,13 @@ def register_user(post_data):
 		register = query_database(query, values)
 		return {"status": "success", "username": user, "email": email}, 200
 
-
-# @csrf_exempt
 def login_user(post_data):
 	try:
 		user = post_data['user']
 		password = post_data['password']
 	except KeyError:
 		return {"error": "Invalid key in request"}, 200
-	query = 'SELECT username, email, password, created FROM User WHERE username = %s'
+	query = 'SELECT username, email, password, created, last_visit FROM User WHERE username = %s'
 	values = (user,)
 	users = query_database(query, values)
 	if len(users) == 0:
@@ -100,9 +101,19 @@ def login_user(post_data):
 		else:
 			return {"status": "success"}, 200
 	else:
-		date = users[0][3]
+		# date = users[0][3]
+
 		if not test_password(users[0][2], password):
 			return {"error": "Invalid password"}, 200
+
+		notifications = get_notification(user)
+
+		# TODO: Need to update the user table's last_visit after getting notifications.
+		query = 'UPDATE User SET last_visit = %s WHERE username = %s AND password = %s'
+		date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+		values = (date, user, password)
+		query_result = query_database(query, values)
+
 		query = 'SELECT * FROM Location WHERE owner = %s'
 		values = (user,)
 		locations = query_database(query, values)
@@ -124,7 +135,7 @@ def login_user(post_data):
 					"notes": location[6]
 				}
 				data.append(loc_data)
-			return {'user': user_data, 'locations': data}, 200
+			return {'user': user_data, 'locations': data, 'notifications': notifications}, 200
 		except KeyError as e:
 			logging.warning("login_user key error: {}".format(e))
 			return {"error": "Invalid key in database data"}, 200
@@ -133,8 +144,7 @@ def login_user(post_data):
 			return {'user': user_data, 'locations': None}, 200
 
 
-# @csrf_exempt
-# @require_POST
+
 def add_location(post_data):
 	try:
 		user = post_data['owner']
@@ -157,8 +167,6 @@ def add_location(post_data):
 	else:
 		return {"status": "success"}, 201
 
-
-# @require_GET
 def delete_location(user='', _id=''):
 	query = 'DELETE FROM Location WHERE id = %s AND owner = %s'
 	values = (_id, user,)
@@ -171,9 +179,6 @@ def delete_location(user='', _id=''):
 	else:
 		return {"status": "success"}, 200
 
-
-# @csrf_exempt
-# @require_POST
 def edit_location(post_data):
 	try:
 		user = post_data['owner']
@@ -187,7 +192,6 @@ def edit_location(post_data):
 	values = (name, marked, notes, user, _id,)
 	location = query_database(query, values)
 	return {"status": "success"}, 200
-
 
 def get_location(user='', _id=''):
 	"""
@@ -206,3 +210,73 @@ def get_location(user='', _id=''):
 		return location[0], 200
 	else:
 		return location, 200
+
+
+
+def add_notification():
+	"""
+	After notification call from last sync time.
+	"""
+	pass
+
+def edit_notification():
+	"""
+	Set is_new false after read.
+	"""
+	pass
+
+def delete_notification():
+	"""
+	When "Clear" notifications button is hit.
+	"""
+	pass
+
+def get_notification(user):
+	"""
+	Populates the notifications list.
+	Populate with all notifications if new user / registered??
+	"""
+	# get notifications since last_visit, then update last_visit
+	notifications = get_all_notifications()  # gets all notifications at /cyan/cyano/notifications
+
+	# TODO: Update user's notifications table with notifications.
+	query = 'INSERT INTO Notifications (owner, id, date, subject, body, is_new) VALUES (%s, %s, %s, %s, %s, %s)'
+	values = []
+	# Consider refactoring for improved computation time.
+	for notification in notifications:
+		# values.append((user, None, convert_to_timestamp(notification['dateSent']), notification['subject'], notification['message'], 1))
+		values.append((user, notification['id'], convert_to_timestamp(notification['dateSent']), notification['subject'], notification['message'], 1))
+	notifications = query_database(query, values, 'multi')
+
+	# return notifications  # returns DB array of notifications
+	return values
+
+	
+
+def get_all_notifications():
+	"""
+	Gets all notifications from epa cyano endpoint.
+	"""
+	notification_url = 'https://cyan.epa.gov/cyan/cyano/notifications/'
+	start_date = '2017-06-23T00-00-00-000-0000'
+	try:
+		notification_response = requests.get(notification_url + start_date)
+		return json.loads(notification_response.content)
+	except requests.exceptions.Timeout:
+		logging.warning("Request to {} timed out.".format(notification_url))
+		# TODO: Retry request.
+		return None
+	except requests.exceptions.RequestException:
+		logging.warning("Error making request to {}.".format(notification_url))
+		# TODO: Handle error.
+		return None
+	except Exception as e:
+		logging.warning("Unknown exception occurred: {}".format(e))
+		# TODO: Handle error.
+		return None
+
+
+def convert_to_timestamp(unix_time):
+	mytime = time.time()
+	trimmed_time = int(str(unix_time)[:-3])
+	return datetime.datetime.fromtimestamp(trimmed_time).strftime('%Y-%m-%d %H:%M:%S')
