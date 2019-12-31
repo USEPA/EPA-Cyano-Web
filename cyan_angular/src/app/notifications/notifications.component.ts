e6fd5fb644b71185206440691487b02fd8b12faa
popenc <i.nickpope@gmail.com>
2019-12-31 15:52:02 -0500

    updates for notifications component - white/gray backgrouns for old/new notifications, 'new notifications' counter only including new notifications

--- cyan_angular/src/app/notifications/notifications.component.ts
+++ cyan_angular/src/app/notifications/notifications.component.ts
@@ -7,6 +7,7 @@ import { Location } from '../models/location';
 // import { LocationService } from '../services/location.service';
 // import { MapService } from '../services/map.service';
 import { UserService } from '../services/user.service';
+import { DownloaderService } from '../services/downloader.service';
 
 @Component({
   selector: 'app-notifications',
@@ -16,22 +17,28 @@ import { UserService } from '../services/user.service';
 export class NotificationsComponent implements OnInit {
 
 	new_notifications = [];
+  new_notifications_counter: number = 0;
+
+  showNotification: boolean = false;  // shows notification detail
+
 	notificationSubscription: Subscription;
 
   constructor(
 		// private router: Router,
-		private userService: UserService
+		private userService: UserService,
+    // private downloaderService: DownloaderService
   ) { }
 
   ngOnInit() {
 
 		this.new_notifications = this.userService.currentAccount.notifications;
+    this.new_notifications_counter = this.new_notifications.filter(x => x[5] === 1).length;
 
-  	// Is this needed?
 		this.notificationSubscription = this.userService.allNotifications$.subscribe(
 			notifications => {
 				console.log("Received notifications update in notifications component.");
-				this.new_notifications = notifications;
+        this.new_notifications = notifications;
+        this.new_notifications_counter = notifications.filter(x => x[5] === 1).length;
 			}
 		);
   }
@@ -46,12 +53,23 @@ export class NotificationsComponent implements OnInit {
   hasNotifications() {
   }
 
-  notificationSelect() {
+  notificationSelect(notification) {
   	/*
   	Check status of notification and set bool based on
   	is_new Notification attribute.
   	*/
     console.log("notificationSelect() function hit.");
+
+    if (notification[5] == 1) {
+      notification[5] = 0;  // sets is_new to false
+
+      let owner = notification[0];
+      let id = notification[1];
+
+      let notifications = this.userService.updateUserNotifications(owner, id);
+
+    }
+
   }
 
   clearNotifications() {
@@ -59,6 +77,10 @@ export class NotificationsComponent implements OnInit {
     Removes all user's notifications.
     */
     console.log("clearNotifications() function hit.");
+    let user = this.userService.getUserName();
+    this.userService.clearUserNotifications(user);
+    // Remove notifications from the list
   }
 
 }
+
