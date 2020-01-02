b861460531afb1293d64b5755ef8f03bd4b12b84
popenc <i.nickpope@gmail.com>
2020-01-02 09:10:44 -0500

    added notification edit and delete endpoints

--- cyan_flask/app.py
+++ cyan_flask/app.py
@@ -176,13 +176,17 @@ class EditNotification(Resource):
 	"""
 	Endpoint for setting is_new false after read.
 	"""
-	pass
+	def get(self, user='', _id=''):
+		results, status_code = web_app_api.edit_notifications(user, _id)
+		return results, status_code
 
 class DeleteNotification(Resource):
 	"""
 	Endpoint for "Clear" notifications.
 	"""
-	pass
+	def get(self, user=''):
+		results, status_code = web_app_api.delete_notification(user)
+		return results, status_code
 
 class GetNotification(Resource):
 	"""
@@ -197,14 +201,14 @@ api.add_resource(StatusTest, '/test')
 api.add_resource(Login, '/cyan/app/api/user')
 api.add_resource(Register, '/cyan/app/api/user/register')
 api.add_resource(AddLocation, '/cyan/app/api/location/add')
-api.add_resource(EditLocation, '/cyan/app/api/location/edit')
+api.add_resource(EditLocation, '/cyan/app/api/location/edit/<string:user>/<string:_id>')
 api.add_resource(DeleteLocation, '/cyan/app/api/location/delete/<string:user>/<string:_id>')
 api.add_resource(GetLocation, '/cyan/app/api/location/<string:user>/<string:_id>')
 
 # Notifications Endpoints:
 api.add_resource(AddNotification, '/cyan/app/api/notification/add')
 api.add_resource(EditNotification, '/cyan/app/api/notification/edit')
-api.add_resource(DeleteNotification, '/cyan/app/api/notification/delete/<string:user>/<string:_id>')
+api.add_resource(DeleteNotification, '/cyan/app/api/notification/delete/<string:user>')
 api.add_resource(GetNotification, '/cyan/app/api/notification/<string:user>/<string:_id>')
 
 
