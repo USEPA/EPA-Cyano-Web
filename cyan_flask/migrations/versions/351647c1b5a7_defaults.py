0234750df654a08e1b022e9102cb602d55862488
fang-yan-csra <fang.yan@csra.com>
2020-05-20 09:59:53 -0400

    Set settings.enalbe_alert default false as well

--- cyan_flask/migrations/versions/351647c1b5a7_defaults.py
+++ cyan_flask/migrations/versions/351647c1b5a7_defaults.py
@@ -8,7 +8,6 @@ Create Date: 2020-05-07 13:33:54.528370
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision = '351647c1b5a7'
 down_revision = 'b66eab762188'
@@ -18,35 +17,45 @@ depends_on = None
 
 def upgrade():
     op.alter_column(
-    	table_name='location',
-    	column_name='marked',
-    	server_default=sa.text('false')
+        table_name='location',
+        column_name='marked',
+        server_default=sa.text('false')
     )
     op.alter_column(
-    	table_name='location',
-    	column_name='compare',
-    	server_default=sa.text('false')
+        table_name='location',
+        column_name='compare',
+        server_default=sa.text('false')
     )
     op.alter_column(
-    	table_name='location',
-    	column_name='type',
-    	server_default=sa.text('1')
+        table_name='location',
+        column_name='type',
+        server_default=sa.text('1')
+    )
+    op.alter_column(
+        table_name='settings',
+        column_name='enable_alert',
+        server_default=sa.text('false')
     )
 
 
 def downgrade():
     op.alter_column(
-    	table_name='location',
-    	column_name='marked',
-    	server_default=None
+        table_name='location',
+        column_name='marked',
+        server_default=None
     )
     op.alter_column(
-    	table_name='location',
-    	column_name='compare',
-    	server_default=None
+        table_name='location',
+        column_name='compare',
+        server_default=None
     )
     op.alter_column(
-    	table_name='location',
-    	column_name='type',
-    	server_default=None
+        table_name='location',
+        column_name='type',
+        server_default=None
+    )
+    op.alter_column(
+        table_name='settings',
+        column_name='enable_alert',
+        server_default=None
     )
