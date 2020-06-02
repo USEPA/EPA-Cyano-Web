dc757406a5df977b892a6ebb050a50236060cd3d
popenc <i.nickpope@gmail.com>
2020-06-02 10:15:01 -0400

    initial loading wheel div with white background, uses progress bar for initial location of user's location data upon login, and just the spinner for location data requests

--- cyan_angular/src/app/services/loader.service.ts
+++ cyan_angular/src/app/services/loader.service.ts
@@ -4,10 +4,19 @@ import { Subject } from "rxjs";
 @Injectable()
 export class LoaderService {
   isLoading = new Subject<boolean>();
+  isUserLocations = new Subject<boolean>();
+  progressValue = new Subject<number>();
   show() {
     this.isLoading.next(true);
   }
   hide() {
     this.isLoading.next(false);
+    this.isUserLocations.next(false);
+  }
+  showProgressBar() {
+  	this.isUserLocations.next(true);
+  }
+  updateProgressValue(value: number) {
+  	this.progressValue.next(value);
   }
 }
--- cyan_angular/src/app/shared/loader/loader.component.ts
+++ cyan_angular/src/app/shared/loader/loader.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Subject } from "rxjs";
+import { MatDialog } from "@angular/material/dialog";
 
 import { LoaderService } from "../../services/loader.service";
 
@@ -11,7 +12,40 @@ import { LoaderService } from "../../services/loader.service";
 export class LoaderComponent {
   color = "primary";
   mode = "indeterminate";
-  isLoading: Subject<boolean> = this.loaderService.isLoading;
+  progress = "0"
 
-  constructor(private loaderService: LoaderService) {}
+  isLoading: Subject<boolean> = this.loaderService.isLoading;
+  isUserLocations: Subject<boolean> = this.loaderService.isUserLocations;
+  progressValue: Subject<number> = this.loaderService.progressValue;
+
+  showProgressBar: boolean = false;
+
+  constructor(
+  	private loaderService: LoaderService,
+  	private matDialog: MatDialog
+  ) {}
+
+  ngOnInit() {
+  	this.progressValue.subscribe(value => {
+  		this.progress = value.toString();
+  	});
+  	this.isUserLocations.subscribe(value => {
+  		this.showProgressBar = value;
+  	});
+  }
+
+  ngOnDestroy() {
+  	console.log("loader component ngOnDestroy called.");
+  	this.progressValue.next(0);
+  }
+
+  exitLoadWheel() {
+  	/*
+  	Removes loading wheel from blocking.
+  	*/
+  	// TODO: Is it possible to cancel requests? They've already been made (looped ajax), so probably not unless they're rate-limited
+  	this.isLoading.next(false);
+  	this.progressValue.next(0);
+  }
+
 }
