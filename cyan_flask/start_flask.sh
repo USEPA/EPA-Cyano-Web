ed83126350959b6dc0c93367990b1c95c27b8050
popenc <i.nickpope@gmail.com>
2020-09-01 11:08:01 -0400

    added integration tests for api-db interactions; added integration tests to start_flask.sh for docker stack

--- cyan_flask/start_flask.sh
+++ cyan_flask/start_flask.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-python -m unittest
+python -m unittest  # runs unit tests (everything in tests/ with test_* pattern)
+python -m unittest tests/integration_tests.py  # runs integration tests (tests/integration_tests.py)
 flask db-user  # creates user if not exist
-flask db-upgrade  # applies db migrations
+flask db-upgrade  # applies db migrations, creates db and tables if not exist
 exec uwsgi --ini /etc/uwsgi/uwsgi.ini  # runs flask
