817b3aae018d71cceb12b1ed69f4e09293efb536
popenc <i.nickpope@gmail.com>
2021-04-19 08:10:15 -0400

    using randomly generated pass for initial cyan-db setup; removed long-running setup from start_flask.sh

--- docker/flask/start_flask.sh
+++ docker/flask/start_flask.sh
@@ -1,34 +1,17 @@
 #!/bin/bash
 
-# export FLASK_APP=/cyan_flask/app
-
 cd /cyan_flask  # switch to cyan_flask directory
 
-python -m unittest  # runs unit tests
+# Runs unit tests:
+# python -m unittest
 
-python -m unittest tests/integration_tests.py  # runs integration tests (tests/integration_tests.py)
+# Runs integration tests (tests/integration_tests.py):
+# python -m unittest tests/integration_tests.py
 
-# echo "~~~~~ Current environment: ~~~~~"
-# printenv  # env is loaded for docker-compose 'env_file'
-# echo "~~~~~~~~~~~~~~~~~~~~"
-
-# 1. Changes root pass for db root@localhost
-echo "### Updating root password for db at localhost"
-flask user-update root localhost $MYSQL_ROOT_PASSWORD $DB_ROOT_PASS  # sets root@localhost to pass from .env file
-
-# 2. Changes root pass for api manage.py (root@%)
-echo "### Updating root password for database at %"
-flask user-update root % $MYSQL_ROOT_PASSWORD $DB_ROOT_PASS  # sets root@% to pass from .env file (handles cyan-api startup db migrations)
-
-export MYSQL_ROOT_PASSWORD=  # unsets mysql root pass
-
-# 3. Creates cyano user for API-DB interactions
-echo "### Creating user: $DB_USER, if not exist."
-flask user-create $DB_USER % $DB_ROOT_PASS $DB_PASS  # using new root pass to create cyano db user
-
-# 4. Performs any database migrations
+# Performs any database migrations
 echo "### Performing database migrations."
 flask db-upgrade  # applies db migrations
 
-# 5. Starts Flask server
+# Starts Flask server
+echo "### Starting flask uwsgi server."
 exec uwsgi --ini /etc/uwsgi/uwsgi.ini  # runs flask
