3c8159e897d0a8b980a7e13afe40a9ad6e066e5f
popenc <i.nickpope@gmail.com>
2021-04-20 13:27:35 -0400

    styling update for db setup instructions

--- docker/mysql/get_temp_pass.sh
+++ docker/mysql/get_temp_pass.sh
@@ -4,9 +4,9 @@ docker-compose logs cyan-db 2>&1 | grep GENERATED
 echo
 echo "1. Copy randomly generated password for updating DB"
 echo
-echo "2. Run docker-compose exec cyan-db mysql -uroot -p"
+echo "2. Run 'docker-compose exec cyan-db mysql -uroot -p'"
 echo
 echo "3. Use password from 1. to log into mysql session."
 echo
-echo "4. Update root password for environment: set password for 'root'@'<the host>' = '<the password>';"
+echo "4. Update root password for environment: 'set password for 'root'@'<the host>' = '<the password>';'"
 echo
