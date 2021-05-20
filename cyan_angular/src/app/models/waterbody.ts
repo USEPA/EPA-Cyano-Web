3ec5b1721bfa84c159a747a91d895e1880bee962
popenc <i.nickpope@gmail.com>
2021-05-20 16:15:07 -0400

    added waterbody model; added waterbody stats details template

---
+++ cyan_angular/src/app/models/waterbody.ts
@@ -0,0 +1,13 @@
+export class WaterBody {
+	objectid: number;
+	name: string;
+	centroid_lat: number;
+	centroid_lng: number;
+}
+
+export class WaterBodyData {
+	objectid: number;
+	daily: boolean;
+	data: any;
+}
+
