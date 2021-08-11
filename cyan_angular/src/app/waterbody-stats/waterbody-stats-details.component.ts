5f8e41c8efee0b36724104abe8fa5a3cd058e18e
popenc <i.nickpope@gmail.com>
2021-08-11 16:07:25 -0400

    more fixes for ordering

--- cyan_angular/src/app/waterbody-stats/waterbody-stats-details.component.ts
+++ cyan_angular/src/app/waterbody-stats/waterbody-stats-details.component.ts
@@ -262,25 +262,14 @@ export class WaterBodyStatsDetails {
       this.dialog.handleError('No ' + this.selectedDataType + ' waterbody data currently available');
     }
 
-    // this.curatedData[dataType]['dates'] = Object.keys(this.waterbodyData[dataType]['data']);
-    // this.curatedData[dataType]['formattedDates'] = this.curatedData[dataType].dates.map(date => {
-    //   return this.calcs.getDateFromDayOfYear(date);
-    // });
-
-    console.log("calculateAllWaterbodyStats() called, available dates: ")
-    console.log(this.waterbodyData[dataType]['data'])
-    console.log(this.curatedData[dataType]['dates'])
-    console.log(this.curatedData[dataType]['formattedDates'])
-
-    if (this.curatedData[dataType]['dates'].length < 1) {
+    if (Object.keys(this.waterbodyData[dataType]['data']).length < 1) {
       this.dialog.handleError('No ' + this.selectedDataType + 'data found for waterbody');
     }
 
     this.curatedData[dataType]['dates'] = [];
     this.curatedData[dataType]['formattedDates'] = [];
 
-    // this.curatedData[dataType].dates.forEach((date, index) => {
-    this.waterbodyData[dataType].forEach((date, index) => {
+    for (let date in this.waterbodyData[dataType]['data']) {
       this.curatedData[dataType][date] = {
         stats: null,  // waterbody stats
         data: null  // curated data for plots, divided by range
@@ -297,9 +286,9 @@ export class WaterBodyStatsDetails {
       let chartData = this.createChartData(concentrationData);  // data by range for date
       this.curatedData[dataType][date].stats = wbStats;
       this.curatedData[dataType][date].data = chartData;
-      this.curatedData[dataType][date].dates.push(date);
-      this.curatedData[dataType][date].formattedDates.push(wbStats.date);
-    });
+      this.curatedData[dataType].dates.push(date);
+      this.curatedData[dataType].formattedDates.push(wbStats.date);
+    }
 
   }
 
