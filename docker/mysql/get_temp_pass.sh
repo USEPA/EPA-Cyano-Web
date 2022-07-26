e6e62ed1874d38cc10ca4594e7e74455ff28a25c
popenc <i.nickpope@gmail.com>
2022-07-26 09:27:45 -0400

    pending updates on dev-kube

--- docker/mysql/get_temp_pass.sh
+++ docker/mysql/get_temp_pass.sh
@@ -8,5 +8,5 @@ echo "2. Run 'docker-compose exec cyan-db mysql -uroot -p'"
 echo
 echo "3. Use password from 1. to log into mysql session."
 echo
-echo "4. Update root password for environment: 'set password for 'root'@'<the host>' = '<the password>';'"
+echo "4. Update root password for environment: 'set password for 'root'@'<the host>' = '<the password>';' or 'SET PASSWORD FOR 'root'@'localhost' = PASSWORD('New_Password');'"
 echo
