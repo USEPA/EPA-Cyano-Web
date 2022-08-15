72b2526bbb64c78b89eea638c45afe8bc0d334a7
popenc <i.nickpope@gmail.com>
2022-08-15 10:32:26 -0400

    conditionally loads .env file if it exists

--- config/set_environment.py
+++ config/set_environment.py
@@ -10,6 +10,7 @@ import platform
 import requests
 import json
 import socket
+import os.path
 
 # logger = logging.getLogger(__name__)
 # logger.warning("EPA-Cyano-Web set_environment.py")
@@ -27,5 +28,8 @@ class DeployEnv:
 
     def load_deployment_environment(self):
         # TODO: Load default .env if .env not found?
-        logging.warning("Loading env vars from: {}.".format(self.env_path))
-        load_dotenv(self.env_path)  # loads env vars into environment
+        if os.path.exists(self.env_path):
+            logging.info("Loading env vars from: {}.".format(self.env_path))
+            load_dotenv(self.env_path)  # loads env vars into environment
+        else:
+            logging.warning("Could not locate env var file: {}. Assuming env vars already set in enviornment.".format(self.env_path))
